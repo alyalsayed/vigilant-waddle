6f09d7124abc0ca380a172423472fe4c2e51207a
alyalsayed <alyalsayed56@yahoo.com>
2024-10-22 16:58:26 +0300

    feat: upload db.php config file

---
+++ src/Config/db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'host' => $_ENV['DB_HOST'],
+    'port' => $_ENV['DB_PORT'],
+    'username' => $_ENV['DB_USERNAME'],
+    'password' => $_ENV['DB_PASSWORD'],
+    'dbname' => $_ENV['DB_NAME'],
+];
