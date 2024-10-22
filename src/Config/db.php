58cf336559a07fb03881da3978c9834ed95d1d59
alyalsayed <alyalsayed56@yahoo.com>
2024-10-22 17:45:23 +0300

    feat: update db.php config

--- src/Config/db.php
+++ src/Config/db.php
@@ -1,8 +1,9 @@
 <?php
 return [
-    'host' => $_ENV['DB_HOST'],
-    'port' => $_ENV['DB_PORT'],
-    'username' => $_ENV['DB_USERNAME'],
-    'password' => $_ENV['DB_PASSWORD'],
+    'host' => $_ENV['DB_HOST'] ?? 'localhost',
+    'port' => $_ENV['DB_PORT'] ?? '3306', 
+    'username' => $_ENV['DB_USERNAME'] ?? 'root',
+    'password' => $_ENV['DB_PASSWORD'] ?? '',
     'dbname' => $_ENV['DB_NAME'],
+    'driver' => $_ENV['DB_CONNECTION'] ?? 'mysql'
 ];
